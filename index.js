482b12810eff47b723a69f0f03881de799400d33
RohanVashisht003 <sharmarohan544@gmail.com>
2022-02-02 16:55:24 +0530

    installed and setup ejs view engine

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+
 // use express router
 app.use('/',require('./routes'));
-
-
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,"views"));
 
 app.listen(port, function(err){
     if(err){
@@ -14,4 +15,4 @@ app.listen(port, function(err){
     else{
         console.log(`Server is running on port ${port}`);
     }
-})
+});
