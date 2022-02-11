d7c0192527be2960186dcf4f143b77f644d6a8f4
RohanVashisht003 <sharmarohan544@gmail.com>
2022-02-11 13:15:02 +0530

    .

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,54 @@
-module.exports.profile = function(req, res){
-    return res.render('users',{
-        title: "Users"
-    });
-}
+const User = require('../models/user');
+
+module.exports.profile = function (req, res) {
+  return res.render("users", {
+    title: "Users",
+  });
+};
+
+// render signup page
+module.exports.signUp = function (req, res) {
+  return res.render("user_signup", {
+    title: "Codeial | Sign Up",
+  });
+};
+
+// render signin page
+module.exports.signIn = function (req, res) {
+  return res.render("user_signin", {
+    title: "Codeial | Sign In",
+  });
+};
+
+// get the sign in data
+module.exports.create = function (req, res) {
+  if(req.body.password!== req.body.confirm_password){
+    return res.redirect('back');
+  }
+
+  User.findOne({email:req.body.email}, function(err,user){
+    if(err){
+      console.log("Error in finding user in signing Up");
+      return;
+    }
+
+    if(!user){
+      User.create(req.body, function(err, user){
+        if(err){
+          console.log("Error in finding user in signing Up");
+          return;
+        }
+        return res.redirect('/users/sign-in');
+      });
+    }
+    else{
+      return res.redirect('back');
+    }
+  })
+};
+
+// sign in and create session for user
+module.exports.createSession = function (req, res) {
+  // ToDo later
+  
+};
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const path = require("path");
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 app.use(express.static("./assets"));
 app.use(expressLayouts);
@@ -13,9 +19,13 @@ app.set("layout extractScripts", true);
 
 // use express router
 app.use("/", require("./routes"));
+
+// setting up view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+
+// for starting server
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error ${err}`);
