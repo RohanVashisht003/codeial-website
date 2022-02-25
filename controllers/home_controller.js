21370a984408a9131120914c45167a04138ac092
RohanVashisht003 <sharmarohan544@gmail.com>
2022-02-25 13:00:59 +0530

    managing comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 const Post = require("../models/post");
 
-module.exports.home = function(req, res){
+module.exports.home = function (req, res) {
     // console.log(req.cookies);
     // res.cookie('user_id',25);
 
@@ -13,10 +13,18 @@ module.exports.home = function(req, res){
 
 
     // populate the user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
-        return res.render('home',{
-            title:"Codeial | Home",
-            posts: posts
+    Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function (err, posts) {
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts: posts
+            });
         });
-});
 }
--- models/post.js
+++ models/post.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    content:{
-        type:String,
-        required:true
+    content: {
+        type: String,
+        required: true
     },
     user: {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
-    }
-},{
-    timestamps:true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    // include the array of Id of comments in Post schema itself
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
+}, {
+    timestamps: true
 });
 
-const Post = mongoose.model('Post',postSchema);
+const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
