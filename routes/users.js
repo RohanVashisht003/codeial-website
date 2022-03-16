5b646e2a452c0faca93e42a5f04eb2ef32e0c8b6
RohanVashisht003 <sharmarohan544@gmail.com>
2022-03-16 15:23:55 +0530

    corrected code

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 
 const userController = require("../controllers/users_controller");
 
-router.get("/profile/:id",passport.checkAuthentication, userController.profile);
+router.get("/profile/:id", passport.checkAuthentication, userController.profile);
 
 router.post("/update/:id", passport.checkAuthentication, userController.update);
 
@@ -14,7 +14,9 @@ router.get("/sign-in", userController.signIn);
 router.post("/create", userController.create);
 
 // use passport as a middleware to authenticate
-router.post("/create-session", passport.authenticate("local", { failureRedirect: "/users/sign-in" }),userController.createSession);
+router.post("/create-session", passport.authenticate("local", {
+    failureRedirect: "/users/sign-in"
+}), userController.createSession);
 
 router.get('/sign-out', userController.destroySession);
 module.exports = router;
