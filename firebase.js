b0791db5fe901459eda2b30bef43ea9c8929284a
RohanVashisht003 <sharmarohan544@gmail.com>
2022-05-27 00:13:11 +0530

    testing firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJTlnheW1nl1IsXlCrQDCqT-cOM0iLqfo",
+  authDomain: "codeial-003.firebaseapp.com",
+  projectId: "codeial-003",
+  storageBucket: "codeial-003.appspot.com",
+  messagingSenderId: "380898498425",
+  appId: "1:380898498425:web:f1d321ede1e8aae15101a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 require('./config/view-helpers')(app);
 const path = require("path");
-const port = env.codeial_main_port;
+const port = env.codeial_main_port || 8000;
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
 // used for session cookies
